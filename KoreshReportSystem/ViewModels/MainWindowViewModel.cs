661a092a80825a692fa8f1230e64af67fb12d95e
ilyaschennikov <ilyaschennikov@yandex.ru>
2021-11-13 13:17:00 +0300

    Init project

---
+++ KoreshReportSystem/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreshReportSystem.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Header { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Header = "KoreshReportSystem";
+        }
+    }
+}
---
+++ KoreshReportSystem/ViewModels/ViewModelBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreshReportSystem.ViewModels
+{
+    /// <summary>
+    /// Базовый класс для ViewModel с уведомлениями
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        #region IDisposable Members
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~ViewModelBase()
+        {
+            this.Dispose(false);
+            //Debug.Fail("Dispose not called on ViewModel class.");
+        }
+
+        /// <summary>
+        /// Освобождение ресурсов
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Освобождение ресурсов
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        /// <summary>
+        /// Событие изменения свойства
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Обработчик события изменения свойства
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                this.EnsureProperty(propertyName);
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        [Conditional("DEBUG")]
+        private void EnsureProperty(string propertyName)
+        {
+            Debug.WriteLine($"PropertyChanged: {propertyName}");
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                throw new ArgumentException("Property does not exist.", "propertyName");
+            }
+        }
+        #endregion
+
+    }
+}
