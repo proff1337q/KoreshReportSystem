c365b44cfa6aa3df4ef481828a8fac3ca6d8a699
ilyaschennikov <ilyaschennikov@yandex.ru>
2021-11-21 15:39:52 +0300

    #5 Added RelayCommand Edited nuget.config

---
+++ KoreshReportSystem/Common/RelayCommand.cs
@@ -0,0 +1,72 @@
+﻿using KoreshReportSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KoreshReportSystem.Common
+{
+    public class RelayCommand : ViewModelBase, ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+        private readonly Func<string> setToolTip;
+
+        private string _toolTip;
+        public string ToolTip
+        {
+            get => _toolTip;
+            set
+            {
+                if (_toolTip != value)
+                {
+                    _toolTip = value;
+                    OnPropertyChanged(nameof(ToolTip));
+                }
+            }
+        }
+
+        #region Constructors
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute, Func<string> setToolTip = null)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+            this.setToolTip = setToolTip;
+            ToolTip = setToolTip?.Invoke();
+        }
+        #endregion
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            ToolTip = setToolTip?.Invoke();
+            return this.canExecute == null ? true : this.canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            //Helper.Unfocus();
+            this.execute(parameter);
+        }
+
+        #endregion
+    }
+}
